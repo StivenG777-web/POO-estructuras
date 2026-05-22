7daa1f35e6319ecbd54411443b37e8f02a439aea
StivenG777-web <guerrerorivasjhonstiven@gmail.com>
2026-05-22 11:32:48 -0500

    uploading All

--- juanitoAlimañaComproUnaAraña.java
+++ juanitoAlimañaComproUnaAraña.java
@@ -1,3 +1,203 @@
-//asdgsdfsaas
+/*
+Tusa (part. Nicki Minaj)
+Tusa (feat. Karol G)
+¿Qué pasó contigo?
+¿Qué pasó contigo?
 
-//
+Dímelo
+Dímelo
+
+(O-O-Ovy On The Drums)
+(O-O-Ovy On The Drums)
+
+
+Ya no tiene excusa
+Ya no tiene excusa
+
+Hoy salió con su amiga
+Hoy salió con su amiga
+
+Dizque pa' matar la tusa (tusa)
+Dizque pa' matar la tusa (tusa)
+
+Que porque un hombre le pagó mal
+Que porque un hombre le pagó mal
+
+
+Está dura y abusa
+Está dura y abusa
+
+Se cansó de ser buena
+Se cansó de ser buena
+
+Ahora es ella quien los usa
+Ahora es ella quien los usa
+
+Que porque un hombre le pagó mal
+Que porque un hombre le pagó mal
+
+
+Ya no se le ve sentimental
+Ya no se le ve sentimental
+
+Dice que por otro man no llora (llora), no
+Dice que por otro man no llora (llora), no
+
+
+Pero si le ponen la canción
+Pero si le ponen la canción
+
+Le da una depresión tonta
+Le da una depresión tonta
+
+Llorando lo comienza a llamar
+Llorando lo comienza a llamar
+
+Pero él la dejó en buzón
+Pero él la dejó en buzón
+
+¿Será porque con otra está?
+¿Será porque con otra está?
+
+Fingiendo que a otra se puede amar
+Fingiendo que a otra se puede amar
+
+
+Pero hice todo este llanto por nada
+Pero hice todo este llanto por nada
+
+Ahora soy una chica mala
+Ahora soy una chica mala
+
+Y ahora pateas y gritas como un bebé grande
+And now you kickin' and screamin', a big toddler
+
+No trates de que tus amigos vengan a mí, a mí
+Don't try to get your friends to come holla, holla
+
+
+Oye, solía estar de bajón
+Ayo, I used to lay low
+
+Nunca estaba en las discos, estaba en lo mío
+I wasn't in the clubs, I was on my JO
+
+Hasta que me di cuenta de que eras un fracaso épico
+Until I realized you're an epic fail
+
+Así que no les digas a tus amigos que todavía soy tu nena
+So don't tell your guys that I'm still your bae, yo'
+
+
+Porque es un nuevo día, estoy en un nuevo lugar
+'Cause it's a new day, I'm in a new place
+
+Consiguiendo nuevas vergas, sentada en una cara nueva (está bien)
+Gettin' some new D, sittin' on a new face (okay)
+
+Porque sé que soy la perra más mala que has conocido
+'Cause I know I'm the baddest bitch that you ever really met
+
+Estás buscando a una perra más malvada y aún no la has encontrado
+You searchin' for a badder bitch, and you ain't met her yet
+
+
+Oye, les digo que se vayan, él quiere aflojar
+Ayo, tell 'em to back off, he wanna slack off
+
+No habrá más llamadas de sexo, tendrás que hacerte la paja
+Ain't no more booty calls, you gotta jack off
+
+Somos Karol G y yo, dejamos que las ratas hablen
+It's me and Karol G, we let them rats talk
+
+No se nos echen encima, porque se nos están tirando
+Don't run up on us, 'cause they lettin' the macks off
+
+
+Pero si le ponen la canción
+Pero si le ponen la canción
+
+Le da una depresión tonta
+Le da una depresión tonta
+
+Llorando lo comienza a llamar
+Llorando lo comienza a llamar
+
+Pero él la dejó en buzón
+Pero él la dejó en buzón
+
+¿Será porque con otra está?
+¿Será porque con otra está?
+
+Fingiendo que a otra se puede amar, eh
+Fingiendo que a otra se puede amar, eh
+
+
+Un shot pa' la pena profunda
+Un shot pa' la pena profunda
+
+Y seguimo' gastando la funda (la funda)
+Y seguimo' gastando la funda (la funda)
+
+Otro shot pa' la mente
+Otro shot pa' la mente
+
+Pa' que el recuerdo no la atormente
+Pa' que el recuerdo no la atormente
+
+
+Ya no le copia nada
+Ya no le copia nada
+
+Su ex ya no vale nada
+Su ex ya no vale nada
+
+Se va pa' la disco y solo quiere perrear (perrear)
+Se va pa' la disco y solo quiere perrear (perrear)
+
+Pero se confunde cuando empieza a tomar (tomar)
+Pero se confunde cuando empieza a tomar (tomar)
+
+
+Ella se cura con rumba
+Ella se cura con rumba
+
+Y el amor pa' la tumba
+Y el amor pa' la tumba
+
+To' los hombre' le zumban (le zumban)
+To' los hombre' le zumban (le zumban)
+
+
+Pero si le ponen la canción
+Pero si le ponen la canción
+
+Le da una depresión tonta (tonta)
+Le da una depresión tonta (tonta)
+
+Llorando lo comienza a llamar
+Llorando lo comienza a llamar
+
+Pero él la dejó en buzón
+Pero él la dejó en buzón
+
+¿Será porque con otra está?
+¿Será porque con otra está?
+
+Fingiendo que a otra se puede amar
+Fingiendo que a otra se puede amar
+
+
+Ey, Karol G (Karol G)
+Ey, Karol G (Karol G)
+
+Nicki Minaj, ey
+Nicki Minaj, ey
+
+La Reina con a la reina
+The Queen with a queen
+
+(O-O-Ovy On The Drums)
+(O-O-Ovy On The Drums)
+*/
